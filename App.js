2a857921b69ea819808543919796bfec34a27140
Cesar <cesar.dutten@extendeal.com>
2019-10-03 12:25:06 -0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,34 @@
+import React, {useState} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
+import {AppLoading} from "expo";
+import MealsNavigator from "./navigation/MealsNavigator";
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  })
+};
+
+export default function App() {
+  const [dataLoaded, setDataLoaded] = useState(false);
+
+  if (!dataLoaded) {
+    return <AppLoading startAsync={fetchFonts}
+                       onFinish={() => setDataLoaded(true)}
+                       onError={(err) => console.log(err)}
+    />;
+  }
+
+  return <MealsNavigator/>;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ screens/FavoritesScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const FavoritesScreen = props => {
+    return (
+        <View>
+
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default FavoritesScreen;
